643e5ad40f31707baca8f2493bdf344c849ef759
Matteo Maronati <me@mattinthe.net>
2018-12-09 22:13:37 +0100

    Up

--- install.sh
+++ install.sh
@@ -3,4 +3,8 @@ if [ ! -n "$SHHH" ]; then
   SHHH=~/.shhh
 fi
 
+if [ -f ~/.zshrc ] || [ -h ~/.zshrc ]; then
+  printf "${YELLOW}Found ~/.zshrc.${NORMAL}";
+fi
+
 env git clone --depth=1 https://github.com/MattAndDev/shhh.git "$SHHH"
